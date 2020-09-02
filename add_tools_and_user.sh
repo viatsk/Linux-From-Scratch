bdb2c25ce3d99e71319221b88feb9b3e9df0c28d
viatsk <viatsk@fastmail.com>
2020-09-02 14:09:24 -0400

    Finish user setup environment

--- add_tools_and_user.sh
+++ add_tools_and_user.sh
@@ -13,4 +13,21 @@ passwd lfs
 chown -v lfs $LFS/tools
 chown -v lfs $LFS/sources 
 
+# set up user's working environment 
+# Create a new bash_profile, which will be the new user's computing environment
+cat > /home/lfs/.bash_profile << "EOF"
+exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash
+EOF
+
+# Set up a bashrc so shell starts in the same state 
+cat > /home/lfs/.bashrc << "EOF"
+set +h
+umask 022
+LFS=/mnt/lfs
+LC_ALL=POSIX
+LFS_TGT=$(uname -m)-lfs-linux-gnu
+PATH=/tools/bin:/bin:/usr/bin
+export LFS LC_ALL LFS_TGT PATH
+EOF
+
 
